972ca420b38a456e6945406e7ab2eb685fc4a596
Fabio Rubio <fabio.rubio@caio.com.br>
2024-02-12 16:24:21 -0300

    feat: blog container

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ image, date, title }) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={image} alt='blog image' />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h1>{title}</h1>
+        </div>
+        <p>Read full article</p>
+      </div>
+    </div>
   )
 }
 
